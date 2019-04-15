67ae7cdb6c25d1c17d515e77ffd5de8ed70a58ff
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-04-15 17:31:26 +0200

    Location

---
+++ quest-android-location/app/src/main/java/fr/wildcodeschool/location/MainActivity.java
@@ -0,0 +1,114 @@
+package fr.wildcodeschool.location;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+        private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 4322;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        checkLocationPermission();
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        FusedLocationProviderClient fusedLocationClient =  LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    lastKnownLocation(location);
+                }
+            }
+        });
+        LocationManager locationManager = (LocationManager) this.getSystemService(MainActivity.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                upDateLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted : ask for it
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            // Permission granted
+            getLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Permission granted
+                    getLocation();
+                } else {
+                    // permission denied
+                }
+                break;
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void lastKnownLocation(){
+
+    }
+
+    private  void upDateLocation(Location location){
+        TextView mTvLocation = findViewById(R.id.tvLocation);
+        Toast.makeText(MainActivity.this, "Update location : " + location.toString(), Toast.LENGTH_SHORT).show();
+        mTvLocation.setText("Update location : " + location.toString());
+    }
+
+    private void lastKnownLocation(Location location){
+        TextView mTvLocation = findViewById(R.id.tvLocation);
+        Toast.makeText(MainActivity.this, "Last known location : " + location.toString(), Toast.LENGTH_SHORT).show();
+        mTvLocation.setText("Last known location : " + location.toString());
+    }
+
+}
